d780b31cd022d0ffb37bd540bab1c09fd1417de8
“red” <karatasmert255@gmail.com>
2023-12-26 14:13:09 +0300

    Update About page bio text

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -7,9 +7,16 @@ const About = () => {
         <div className="bio-text">
           <h1 className="bio-title">BIO</h1>
           <p className="bio-pha">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-            obcaecati laborum praesentium ex eveniet, sequi necessitatibus saepe
-            nam qui quaerat debitis, minus consectetur inventore alias!
+            I put the rich layers of my Turkish Language and Literature
+            education into practice with my teaching internship, and in this
+            process, my deep love for education and literature was reinforced.
+            However, my great curiosity for technology led me to new horizons,
+            to Front-End Developer training. The basic skills I acquired there,
+            such as HTML, CSS and JavaScript, laid a solid foundation for me to
+            take an active role in the digital world. This journey, which
+            combines the depth of literature with the innovative dynamism of
+            technology, enlightens my creativity and analytical thinking,
+            enlightening me to achieve my professional goals.
           </p>
         </div>
       </section>
